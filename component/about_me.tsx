b01ded71e266eb32fc7ff83a321053c0d56acf91
Adisorn Parama <61809305+DarkTouiZ@users.noreply.github.com>
2023-11-03 20:01:26 +0700

    add about me page

---
+++ component/about_me.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function ABOUT_ME() {
+    return (
+        <div >
+            <div className="">
+                <img src="https://i.pinimg.com/564x/e3/73/c9/e373c99f3c4b1897f70f51983a14ce8e.jpg" alt="Adisorn Picture" width="300" height="300"></img>
+            </div>
+            <div id="about_me">
+                <h1>
+                    About Me
+                </h1>
+                <h2>
+                    Name: Adisorn Parama 
+                    Birth: 06 AUG. 2004's
+                </h2>
+            </div>
+        </div>
+        
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import {Button} from '@nextui-org/button'; 
 import NavBar from '../../component/navbar';
+import ABOUT_ME from '../../component/about_me';
 
 export default function Home() {
   return (
     
     <div>
       <NavBar/>
+      <ABOUT_ME/>
     </div>
   )
 }
